81db1f0f086542484a44ec4c9b7a09a4ac13ce74
mseneque <mattseneque@gmail.com>
2017-08-24 15:21:39 +0800

    Initial Comit

---
+++ StackOverflow-Post/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace StackOverflow_Post
+{
+    public class Post
+    {
+        public enum State { posted }
+
+        private DateTime _dateCreated;
+        private int _votes;
+
+        // Getters and Setters
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DateTime DateCreated
+        {
+            get { return _dateCreated; }
+            private set { _dateCreated = DateTime.Now; }
+        }
+
+        public int Votes
+        {
+            get { return _votes; }
+        }
+
+        // Default Constructor (parameterless)
+        public Post()
+        {
+            _votes = 0;
+            DateCreated = DateCreated; 
+        }
+
+
+        // Public Methods
+        public void UpVote()
+        {
+            _votes++;
+        }
+
+        public void DownVote()
+        {
+            if (_votes > 0)
+                _votes--;
+        }
+
+
+    }
+}
---
+++ StackOverflow-Post/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post()
+            {
+                Title = "This is the Post Title",
+                Description = "And this is the description text"
+            };
+
+            Console.WriteLine("Title: {0}\nDescription: {1}\nDate created: {2}\nTotal votes: {3}", post.Title, post.Description, post.DateCreated, post.Votes);
+
+            // Down-vote
+            post.DownVote();
+            Console.WriteLine("Down-vote\nTotal Votes: {0}", post.Votes);
+
+            // Three Up-votes
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            Console.WriteLine("Three Up-votes\nTotal Votes: {0}", post.Votes);
+
+            // Down-vote
+            post.DownVote();
+            Console.WriteLine("Down-vote\nTotal Votes: {0}", post.Votes);
+
+            // Try to modify the Created date
+            Console.WriteLine("Date created: {0}", post.DateCreated);
+            System.Threading.Thread.Sleep(2000); // wait a few seconds
+            //post.DateCreated = DateTime.Now;
+            Console.WriteLine("Date created: {0}", post.DateCreated);
+
+        }
+    }
+}
